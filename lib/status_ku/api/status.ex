7fb7abe5edbd270d2d759684c2e4da3d79ea51d5
Anthony Smith <anthony@sticksnleaves.com>
2016-11-05 17:47:08 -0400

    Moved comments to StatusKu module

--- lib/status_ku/api/status.ex
+++ lib/status_ku/api/status.ex
@@ -5,25 +5,16 @@ defmodule StatusKu.API.Status do
 
   import StatusKu.API.Base
 
-  @doc """
-  Retrieve the current status.
-  """
   def current_status do
     request(:get, "/current-status")
     |> StatusKu.Parser.parse_status
   end
 
-  @doc """
-  Retrieve a specific issue.
-  """
   def issue(id) do
     request(:get, "/issues/#{id}")
     |> StatusKu.Parser.parse_single_issue
   end
 
-  @doc """
-  Retrieve a list of issues.
-  """
   def issues(params \\ %{}) do
     request(:get, "/issues", params)
     |> StatusKu.Parser.parse_multiple_issues
