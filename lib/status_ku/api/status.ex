cee42da3c7f6f2af71eccbab2a1b0987c820949c
Anthony Smith <anthony@sticksnleaves.com>
2016-11-05 18:44:54 -0400

    Pattern matching the things?

--- lib/status_ku/api/status.ex
+++ lib/status_ku/api/status.ex
@@ -6,17 +6,14 @@ defmodule StatusKu.API.Status do
   import StatusKu.API.Base
 
   def current_status do
-    request(:get, "/current-status")
-    |> StatusKu.Parser.parse_status
+    {:ok, request(:get, "/current-status") |> StatusKu.Parser.parse_status}
   end
 
   def issue(id) do
-    request(:get, "/issues/#{id}")
-    |> StatusKu.Parser.parse_single_issue
+    {:ok, request(:get, "/issues/#{id}") |> StatusKu.Parser.parse_single_issue}
   end
 
   def issues(params \\ %{}) do
-    request(:get, "/issues", params)
-    |> StatusKu.Parser.parse_multiple_issues
+    {:ok, request(:get, "/issues", params) |> StatusKu.Parser.parse_multiple_issues}
   end
 end
